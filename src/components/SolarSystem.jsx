edb6157510e0c316d7952a4eb008a95abe3aff18
Sergio Francisco <spinheiro.lf@gmail.com>
2022-02-18 16:49:54 -0300

    renderizando planetas

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
-        <Title headline="Planetas" />
-      </div>
+      <>
+        <div data-testid="solar-system">
+          <Title headline="Planetas" />
+        </div>
+        <div>
+          {planets.map((planet) => (
+            <div key={ planet.name }>
+              <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+            </div>))}
+        </div>
+
+      </>
     );
   }
 }
