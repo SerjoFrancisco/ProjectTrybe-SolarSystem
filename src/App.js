6694be7cae45a2bfe3dd8bd388924fe8930fc32c
Sergio Francisco <spinheiro.lf@gmail.com>
2022-02-18 16:33:42 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import planets from './data/planets';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
     return (
-      <body>
+      <>
         <Header />
         <SolarSystem />
-      </body>
+        <PlanetCard />
+      </>
     );
   }
 }
