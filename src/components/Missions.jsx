864c84aa45bb3702db693a147710787ffb7eb4bd
Sergio Francisco <spinheiro.lf@gmail.com>
2022-02-18 17:08:50 -0300

    gerando cards de missao

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import missions from '../data/missions';
 import Title from './Title';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
-        <Title headline="Missões" />
-      </div>
+      <>
+        <div data-testid="missions">
+          <Title headline="Missões" />
+        </div>
+        <div>
+          {missions.map((mission) => (
+            <div key={ mission.name }>
+              <MissionCard
+                name={ mission.name }
+                year={ mission.image }
+                country={ mission.country }
+                destination={ mission.destination }
+              />
+            </div>))}
+        </div>
+      </>
     );
   }
 }
